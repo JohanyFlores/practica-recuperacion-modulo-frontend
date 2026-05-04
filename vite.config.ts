fec4cd88f5939d79032ec375be3ab92213e63b13
JohanyFlores <jfloresm1@student.obsbusiness.school>
2026-05-03 20:26:56 -0600

    feat: configuración inicial del proyecto con Vite, React y Vitest

--- vite.config.ts
+++ vite.config.ts
@@ -4,4 +4,9 @@ import react from '@vitejs/plugin-react'
 // https://vite.dev/config/
 export default defineConfig({
   plugins: [react()],
+  // Configuración para GitHub Pages
+  // Cambia 'practica-recuperacion-modulo-frontend' por el nombre de tu repositorio
+  base: process.env.NODE_ENV === 'production' 
+    ? '/practica-recuperacion-modulo-frontend/' 
+    : '/',
 })
