cc80ab3359cdec53479f2a320b1c88863653d377
JohanyFlores <jfloresm1@student.obsbusiness.school>
2026-05-03 22:25:16 -0600

    feat: configurar proyecto para deploy en Vercel

--- vite.config.ts
+++ vite.config.ts
@@ -4,9 +4,6 @@ import react from '@vitejs/plugin-react'
 // https://vite.dev/config/
 export default defineConfig({
   plugins: [react()],
-  // Configuración para GitHub Pages
-  // Cambia 'practica-recuperacion-modulo-frontend' por el nombre de tu repositorio
-  base: process.env.NODE_ENV === 'production' 
-    ? '/practica-recuperacion-modulo-frontend/' 
-    : '/',
+  // Si estás en Vercel, usa '/', si es GitHub Pages, usa el path del repo
+  base: process.env.VERCEL ? '/' : '/practica-recuperacion-modulo-frontend/',
 })
